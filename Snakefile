a060db4aaa3449f9553debd52fd122f4720b429e
Pat Schloss <pschloss@umich.edu>
2022-10-04 09:33:34 -0400

    Collect time span in years for each weather station

--- Snakefile
+++ Snakefile
@@ -5,7 +5,7 @@ rule targets:
         "data/ghcnd-inventory.txt",
         "data/ghcnd-stations.txt",
         "data/ghcnd_tidy.tsv.gz",
-        "data/ghcnd_regions.tsv"
+        "data/ghcnd_regions_years.tsv"
 
 
 rule get_all_archive:
@@ -67,12 +67,12 @@ rule summarize_dly_files:
         {input.bash_script}
         """
  
-rule aggregate_stations:
+rule get_regions_years:
     input:
-        r_script = "code/merge_lat_long.R",
-        data = "data/ghcnd-stations.txt"
+        r_script = "code/get_regions_years.R",
+        data = "data/ghcnd-inventory.txt"
     output:
-        "data/ghcnd_regions.tsv"
+        "data/ghcnd_regions_years.tsv"
     shell:
         """
         {input.r_script}
--- code/get_regions_years.R
+++ code/get_regions_years.R
@@ -9,29 +9,24 @@ library(tidyverse)
 # ID            1-11   Character
 # LATITUDE     13-20   Real
 # LONGITUDE    22-30   Real
-# ELEVATION    32-37   Real
-# STATE        39-40   Character
-# NAME         42-71   Character
-# GSN FLAG     73-75   Character
-# HCN/CRN FLAG 77-79   Character
-# WMO ID       81-85   Character
+# ELEMENT      32-35   Character
+# FIRSTYEAR    37-40   Integer
+# LASTYEAR     42-45   Integer
 # ------------------------------
 
-read_fwf("data/ghcnd-stations.txt",
+read_fwf("data/ghcnd-inventory.txt",
          col_positions = fwf_cols(
            id = c(1, 11),
            latitude = c(13, 20),
            longitude = c(22, 30),
-           elevation = c(32, 37),
-           state = c(39, 40),
-           name = c(42, 71),
-           gsn_flag = c(73, 75),
-           hcn_flag = c(77, 79),
-           wmo_id = c(81, 85)
-         ),
-         col_select = c(id, latitude, longitude)) %>%
+           element = c(32, 35),
+           first_year = c(37, 40),
+           last_year = c(42, 45))
+         ) %>%
+       filter(element == "PRCP") %>%
   mutate(latitude = round(latitude, 0),
          longitude = round(longitude, 0)) %>%
   group_by(longitude, latitude) %>%
   mutate(region = cur_group_id()) %>%
-  write_tsv("data/ghcnd_regions.tsv")
+  select(-element) %>%
+  write_tsv("data/ghcnd_regions_years.tsv")
