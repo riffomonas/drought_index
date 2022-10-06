933c54314be449dcf9c630772a0228fe2dab73ee
Pat Schloss <pschloss@umich.edu>
2022-10-06 16:44:28 -0400

    Compute z-scores

--- code/merge_weather_station_data.R
+++ code/merge_weather_station_data.R
@@ -12,4 +12,17 @@ station_data <- read_tsv("data/ghcnd_regions_years.tsv")
 lat_long_prcp <- inner_join(prcp_data, station_data, by = "id") %>%
   filter((year != first_year & year != last_year) | year == 2022) %>% 
   group_by(latitude, longitude, year) %>%
-  summarize(mean_prcp = mean(prcp))
+  summarize(mean_prcp = mean(prcp), .groups = "drop")
+
+this_year <- lat_long_prcp %>%
+  filter(year == 2022) %>%
+  select(-year)
+
+inner_join(lat_long_prcp, this_year, by = c("latitude", "longitude")) %>%
+  rename(all_years = mean_prcp.x,
+         this_year = mean_prcp.y) %>%
+  group_by(latitude, longitude) %>%
+  summarize(z_score = (min(this_year) - mean(all_years)) / sd(all_years),
+         n = n(),
+         .groups = "drop") %>%
+  filter(n >= 50)
